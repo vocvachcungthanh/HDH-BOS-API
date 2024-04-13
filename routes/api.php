43a3c6a86c08d4cdaccc59ac273269df657cd4ee
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2024-04-13 17:47:34 +0700

    fix-validator-department

--- routes/api.php
+++ routes/api.php
@@ -58,6 +58,7 @@ Route::middleware(['auth'])->group(function () {
         Route::post('department-create', [DepartmentController::class, 'create']);
         Route::post('delete-department', [DepartmentController::class, 'deleteDepartment']);
         Route::post('empty-department', [DepartmentController::class, 'emptyTrashDepartment']);
+        Route::post('restore-department', [DepartmentController::class, 'restoreTranshDepartment']);
 
         Route::post('postions/{page}', [PostionController::class, 'index']);
         Route::get('create-code-postion', [PostionController::class, 'createAutoCode']);
